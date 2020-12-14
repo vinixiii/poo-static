813642256cc132dc4196325e3647da83097a3724
Vinícius <vinissfigueiroa@gmail.com>
2020-12-14 10:39:20 -0300

    Atividade concluída

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using poo_static.classes;
+
+namespace poo_static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            float valor = 1f;
+
+            //Chamando classe estática sem precisar instânciar
+            Console.WriteLine($"{valor} Dólar = {Conversor.DolarParaReal(valor)} Reais");
+            Console.WriteLine($"{valor} Real = {Conversor.RealParaDolar(valor):0.00} Dólar");
+            Console.WriteLine($"{valor} Euro = {Conversor.EuroParaReal(valor)} Reais");
+            Console.WriteLine($"{valor} Real = {Conversor.RealParaEuro(valor):0.00} Euro");
+
+            //Exeplo: Math
+            Console.WriteLine($"2^4 = {Math.Pow(2, 4)}");
+        }
+    }
+}
---
+++ classes/Conversor.cs
@@ -0,0 +1,24 @@
+namespace poo_static.classes
+{
+    public class Conversor
+    {
+        private static float cotacaoDolar = 5.07f;
+        private static float cotacaoEuro = 6.16f;
+
+        public static float RealParaDolar(float valorRS) {
+            return valorRS / cotacaoDolar;
+        }
+
+        public static float DolarParaReal(float valorDolar) {
+            return valorDolar * cotacaoDolar;
+        }
+
+        public static float RealParaEuro(float valorRS) {
+            return valorRS / cotacaoEuro;
+        }
+
+        public static float EuroParaReal(float valorEuro) {
+            return valorEuro * cotacaoEuro;
+        }
+    }
+}
